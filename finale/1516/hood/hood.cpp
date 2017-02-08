22c9695d0bfca63d0e710da965038eb11d8bb5ba
askbk <askbk@protonmail.com>
2017-02-08 21:03:47 +0100

    hygget på hood

--- finale/1516/hood/hood.cpp
+++ finale/1516/hood/hood.cpp
@@ -1,29 +1,46 @@
 #include <iostream>
 #include <vector>
-#include <deque>
 #include <algorithm>
-
-bool wayToSort(int A, int B){
-  return A>B;
-}
+#include <map>
 
 int main() {
-  int P, N, F;
-  std::deque<int> person;
+  int P, N, F, bunn, topp;
+  int sum = 0;
+  std::vector<int> person;
+
+  std::cin >> P >> N;
+
+  bunn = (N / 2) - 1;
+  topp = bunn + 1;
+
   for (size_t i = 0; i < N; i++) {
     std::cin >> F;
     person.push_back(F);
   }
-  std::sort(person, wayToSort);
 
-  for (size_t i = 0; i < N; i++) {
-    if (person[i]>P) {
-      person[i].
-    }
+  std::sort(person.begin(), person.end());
 
-    for (size_t j = 0; j < N-i; j++) {
-       else {
+  sum = person[bunn] + person[topp];
 
+  while (sum!=P) {  //mens summen ikke er riktig
+    std::cout << sum << '\n';
+    if (sum<P) {  //for lite
+      if (topp!=(N-1)) {  //topp er ikke på siste element
+        ++topp; //flytt topp til høyre
+        sum += person[topp];
+      } else {
+        if (bunn!=0) {  //bunn er ikke på første element
+          --bunn; //flytt bunn til venstre
+          sum += person[bunn];
+        }
+      }
+
+    } else {  //for mye
+      sum -= person[topp];
+      --topp; //flytt topp til venstre
+      if (bunn!=0) {  //flytt til venstre dersom bunn ikke er første element
+        --bunn;
+        sum += person[bunn];
       }
     }
   }
